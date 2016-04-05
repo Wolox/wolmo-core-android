ccd4afcd5d3bc9338920f77f3965d2b8195c9696
Lucas de la Torre <luksdlt92@gmail.com>
2016-04-05 16:51:10 -0300

    Fresco implemented

--- core/src/main/java/ar/com/wolox/wolmo/core/WoloxApplication.java
+++ core/src/main/java/ar/com/wolox/wolmo/core/WoloxApplication.java
@@ -2,6 +2,9 @@ package ar.com.wolox.wolmo.core;
 
 import android.app.Application;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+
 import ar.com.wolox.wolmo.core.service.WoloxRetrofitServices;
 
 public abstract class WoloxApplication extends Application {
@@ -15,11 +18,19 @@ public abstract class WoloxApplication extends Application {
         sApplication = this;
         sRetrofitServices = getRetrofitServices();
         sRetrofitServices.init();
+        setUpFresco();
         onInit();
     }
 
-    // Here you init the another libs like Fresco
+    // Here you init the another libs
     public abstract void onInit();
+
+    private void setUpFresco() {
+        ImagePipelineConfig config = ImagePipelineConfig.newBuilder(this)
+                .setDownsampleEnabled(true)
+                .build();
+        Fresco.initialize(this, config);
+    }
 
     public abstract WoloxRetrofitServices getRetrofitServices();
 
