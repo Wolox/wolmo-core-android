24814c829f3fda8fa1a986033a173153ee2c9ce7
Federico Ramundo <federico.ramundo@wolox.com.ar>
2016-11-30 14:11:14 -0300

    proguard & dependencies

--- core/src/main/java/ar/com/wolox/wolmo/core/WoloxApplication.java
+++ core/src/main/java/ar/com/wolox/wolmo/core/WoloxApplication.java
@@ -2,9 +2,6 @@ package ar.com.wolox.wolmo.core;
 
 import android.app.Application;
 
-import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.imagepipeline.core.ImagePipelineConfig;
-
 import ar.com.wolox.wolmo.core.service.WoloxRetrofitServices;
 
 public abstract class WoloxApplication extends Application {
@@ -18,19 +15,11 @@ public abstract class WoloxApplication extends Application {
         sApplication = this;
         sRetrofitServices = getRetrofitServices();
         sRetrofitServices.init();
-        setUpFresco();
         onInit();
     }
 
-    // Here you init the another libs
+    // Here you init the other libs
     public abstract void onInit();
-
-    private void setUpFresco() {
-        ImagePipelineConfig config = ImagePipelineConfig.newBuilder(this)
-                .setDownsampleEnabled(true)
-                .build();
-        Fresco.initialize(this, config);
-    }
 
     public abstract WoloxRetrofitServices getRetrofitServices();
 
