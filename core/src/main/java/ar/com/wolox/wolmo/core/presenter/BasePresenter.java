20baa62471cee7d0862b8f2b5f124aa14c0aeb62
Fabian <fabian.alvarez@wolox.com.ar>
2017-04-19 18:20:13 -0300

    Change the BasePresenter to avoid null views

--- core/src/main/java/ar/com/wolox/wolmo/core/presenter/BasePresenter.java
+++ core/src/main/java/ar/com/wolox/wolmo/core/presenter/BasePresenter.java
@@ -22,27 +22,32 @@
 package ar.com.wolox.wolmo.core.presenter;
 
 import android.support.annotation.CallSuper;
-import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
 
 /**
  * Base presenter that provides the view to the specific presenters.
  */
 public class BasePresenter<T> {
 
-    private T mViewInstance;
+    private final T mViewInstance;
     private boolean mViewCreated;
+    private boolean mViewAttached;
 
-    public BasePresenter(T viewInstance) {
-        this.mViewInstance = viewInstance;
+    public BasePresenter(@NonNull T viewInstance) {
+        mViewInstance = viewInstance;
+        mViewAttached = true;
     }
 
     /**
      * Returns the view attached this presenter.
-     * If no view is attached, returns null.
+     * <b>NOTE: </b> You should check if the view is created, or at least attached calling the
+     * methods {@link BasePresenter#isViewCreated()} and
+     * {@link BasePresenter#isViewAttached()} before calling method of the view.
+     * It's not safe to call methods if the view isn't created.
      *
-     * @return View attached
+     * @return Attached view
      */
-    @Nullable
+    @NonNull
     protected final T getView() {
         return mViewInstance;
     }
@@ -53,7 +58,7 @@ public class BasePresenter<T> {
      * @return <b>true</b> if the view is attached, <b>false</b> otherwise.
      */
     public final boolean isViewAttached() {
-        return mViewInstance != null;
+        return mViewAttached;
     }
 
     /**
@@ -71,7 +76,7 @@ public class BasePresenter<T> {
      */
     @CallSuper
     public void detachView() {
-        mViewInstance = null;
+        mViewAttached = false;
         mViewCreated = false;
     }
 
