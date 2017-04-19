889a97b2169df6e788dd453366142a89eea9e9a8
Fabian <fabian.alvarez@wolox.com.ar>
2017-04-19 18:20:13 -0300

    Change the BasePresenter to avoid null views

--- core/src/main/java/ar/com/wolox/wolmo/core/util/NetworkCodeUtils.java
+++ core/src/main/java/ar/com/wolox/wolmo/core/util/NetworkCodeUtils.java
@@ -7,6 +7,7 @@ public class NetworkCodeUtils {
     public static final int ERROR_BAD_REQUEST = 400;
     public static final int ERROR_UNAUTHORIZED = 401;
     public static final int ERROR_FORBIDDEN = 403;
+    public static final int ERROR_NOT_FOUND= 404;
     public static final int ERROR_PRECONDITION_FAILED = 412;
     public static final int ERROR_INTERNAL = 500;
     public static final int ERROR_APP_INTERNAL = 600;
