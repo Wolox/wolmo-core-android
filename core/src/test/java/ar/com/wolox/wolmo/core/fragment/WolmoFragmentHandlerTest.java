14164c36b9aa2a709f5be739661d50d21268c065
Fabian <fabian.alvarez@wolox.com.ar>
2017-07-17 16:34:38 -0300

    Add tests to some wolmo classes

---
+++ core/src/test/java/ar/com/wolox/wolmo/core/fragment/WolmoFragmentHandlerTest.java
@@ -0,0 +1,116 @@
+package ar.com.wolox.wolmo.core.fragment;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+
+import ar.com.wolox.wolmo.core.R;
+import ar.com.wolox.wolmo.core.presenter.BasePresenter;
+import ar.com.wolox.wolmo.core.util.ToastUtils;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Log.class})
+public class WolmoFragmentHandlerTest {
+
+    @Rule public final ExpectedException exception = ExpectedException.none();
+
+    private WolmoFragmentHandler<BasePresenter> mWolmoFragmentHandler;
+    private WolmoFragment mWolmoFragment;
+    private ToastUtils mToastUtils;
+
+    @Before
+    @SuppressWarnings("unchecked")
+    public void beforeTests() {
+        // Mock calls to android.util.Log
+        PowerMockito.mockStatic(Log.class);
+
+        mToastUtils = mock(ToastUtils.class);
+        mWolmoFragment = mock(WolmoFragment.class);
+
+        mWolmoFragmentHandler = new WolmoFragmentHandler<>();
+        mWolmoFragmentHandler.setFragment(mWolmoFragment);
+    }
+
+    @Test
+    public void setWolmoFragmentNotExtendingFragment() {
+        IWolmoFragment wolmoFragment = mock(IWolmoFragment.class);
+        exception.expect(IllegalArgumentException.class);
+        mWolmoFragmentHandler.setFragment(wolmoFragment);
+    }
+
+    @Test
+    public void handlerFinishActivityIfWrongArguments() {
+        FragmentActivity activity = mock(FragmentActivity.class);
+
+        when(mWolmoFragment.handleArguments(any(Bundle.class))).thenReturn(false);
+        when(mWolmoFragment.getActivity()).thenReturn(activity);
+
+        mWolmoFragmentHandler.setFragment(mWolmoFragment);
+        mWolmoFragmentHandler.mToastUtils = mToastUtils;
+
+        mWolmoFragmentHandler.onCreate(null);
+        verify(mToastUtils, times(1)).show(R.string.unknown_error);
+        verify(activity, times(1)).finish();
+    }
+
+    @Test
+    public void onViewCreatedCallsWolmoFragmentMethods() {
+        View mockView = mock(View.class);
+
+        mWolmoFragmentHandler.onViewCreated(mockView, null);
+
+        // Verify that the methods in wolmoFragment are called in order
+        InOrder inOrder = inOrder(mWolmoFragment);
+
+        inOrder.verify(mWolmoFragment, times(1)).setUi(mockView);
+        inOrder.verify(mWolmoFragment, times(1)).init();
+        inOrder.verify(mWolmoFragment, times(1)).populate();
+        inOrder.verify(mWolmoFragment, times(1)).setListeners();
+    }
+
+    @Test
+    public void notifyFragmentOnVisibilityChanged() {
+        mWolmoFragmentHandler.setMenuVisibility(true);
+
+        when(mWolmoFragment.isResumed()).thenReturn(true);
+
+        mWolmoFragmentHandler.onViewCreated(mock(View.class), null);
+        mWolmoFragmentHandler.onResume();
+        verify(mWolmoFragment, times(1)).onVisible();
+        verify(mWolmoFragment, times(0)).onHide();
+
+        // Hide the fragment
+        when(mWolmoFragment.isResumed()).thenReturn(false);
+        mWolmoFragmentHandler.onPause();
+        verify(mWolmoFragment, times(1)).onHide();
+    }
+
+    @Test
+    public void detachesPresenterOnDestroyView() {
+        BasePresenter presenter = mock(BasePresenter.class);
+
+        mWolmoFragmentHandler.mPresenter = presenter;
+
+        mWolmoFragmentHandler.onDestroyView();
+        verify(presenter, times(1)).detachView();
+    }
+
+}
---
+++ core/src/test/java/ar/com/wolox/wolmo/core/presenter/BasePresenterTest.java
@@ -0,0 +1,74 @@
+package ar.com.wolox.wolmo.core.presenter;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BasePresenterTest {
+
+    private BasePresenter<Object> mBasePresenter;
+
+    @Before
+    @SuppressWarnings("unchecked")
+    public void beforeTests() {
+        mBasePresenter = spy(BasePresenter.class);
+    }
+
+
+    @Test
+    public void viewAttachedUpdates() {
+        assertThat(mBasePresenter.isViewAttached()).isEqualTo(false);
+
+        mBasePresenter.attachView(new Object());
+        assertThat(mBasePresenter.isViewAttached()).isEqualTo(true);
+
+        mBasePresenter.detachView();
+        assertThat(mBasePresenter.isViewAttached()).isEqualTo(false);
+    }
+
+    @Test
+    public void presenterNotifiesChildClasses() {
+        // Attach and detach the view
+        mBasePresenter.attachView(new Object());
+        verify(mBasePresenter, times(1)).onViewAttached();
+        verify(mBasePresenter, times(0)).onViewDetached();
+
+        mBasePresenter.detachView();
+        verify(mBasePresenter, times(1)).onViewDetached();
+    }
+
+    @Test
+    public void presenterRunIfViewAttachedRunnable() {
+        Runnable runnableMock = mock(Runnable.class);
+
+        mBasePresenter.runIfViewAttached(runnableMock);
+        verify(runnableMock, times(0)).run();
+
+        // Now the view is attached
+        mBasePresenter.attachView(new Object());
+        mBasePresenter.runIfViewAttached(runnableMock);
+        verify(runnableMock, times(1)).run();
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void presenterRunIfViewAttachedConsumer() {
+        BasePresenter.Consumer<Object> consumerMock = mock(BasePresenter.Consumer.class);
+
+        mBasePresenter.runIfViewAttached(consumerMock);
+        verify(consumerMock, times(0)).accept(any(Object.class));
+
+        // Now the view is attached
+        mBasePresenter.attachView(new Object());
+        mBasePresenter.runIfViewAttached(consumerMock);
+        verify(consumerMock, times(1)).accept(any(Object.class));
+    }
+
+}
