8477b2dbb6dbe3d7f2d522402dcd1f2be002dd11
Dylan Muszel <dylan.muszel@wolox.com.ar>
2019-12-17 10:00:34 -0300

    Reverting modules

--- core/src/main/java/ar/com/wolox/wolmo/core/di/modules/ContextModule.java
+++ core/src/main/java/ar/com/wolox/wolmo/core/di/modules/ContextModule.java
@@ -19,32 +19,34 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package ar.com.wolox.wolmo.core.di.modules
+package ar.com.wolox.wolmo.core.di.modules;
 
-import android.app.Activity
-import android.app.Application
-import android.content.Context
-import android.content.SharedPreferences
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 
-import ar.com.wolox.wolmo.core.di.scopes.ApplicationScope
+import ar.com.wolox.wolmo.core.di.scopes.ApplicationScope;
 
-import dagger.Module
-import dagger.Provides
+import dagger.Module;
+import dagger.Provides;
 
 /**
- * Provides objects that depends of the Application [Context].
- * The objects provided by this module uses [ApplicationScope].
+ * Provides objects that depends of the Application {@link Context}.
+ * The objects provided by this module uses {@link ApplicationScope}.
  */
 @Module
-class ContextModule {
+public class ContextModule {
 
-    @Provides
-    @ApplicationScope
-    fun provideContext(application: Application): Context = application.applicationContext
+	@Provides
+	@ApplicationScope
+	Context provideContext(Application application) {
+		return application.getApplicationContext();
+	}
 
-    @Provides
-    @ApplicationScope
-    fun provideSharedPreferences(sharedPrefName: String, context: Context): SharedPreferences {
-        return context.getSharedPreferences(sharedPrefName, Activity.MODE_PRIVATE)
-    }
+	@Provides
+	@ApplicationScope
+	SharedPreferences provideSharedPreferences(String sharedPrefName, Context context) {
+		return context.getSharedPreferences(sharedPrefName, Activity.MODE_PRIVATE);
+	}
 }
