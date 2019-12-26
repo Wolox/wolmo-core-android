7353a146e9fac741711d8cbbc3471530dc536913
Dylan Muszel <dylan.muszel@wolox.com.ar>
2019-12-26 18:34:49 -0300

    Reverting image provider

--- core/src/main/java/ar/com/wolox/wolmo/core/util/ImageProvider.kt
+++ core/src/main/java/ar/com/wolox/wolmo/core/util/ImageProvider.kt
@@ -32,7 +32,6 @@ import android.provider.MediaStore
 import androidx.annotation.IntRange
 import androidx.annotation.StringDef
 import androidx.annotation.StringRes
-import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import ar.com.wolox.wolmo.core.di.scopes.ApplicationScope
 import java.io.ByteArrayOutputStream
@@ -111,7 +110,8 @@ class ImageProvider @Inject constructor(private val context: Context, private va
             return false
         }
 
-        val photoFileUri = File(file).toUri()
+        val photoFile = File(file)
+        val photoFileUri = wolmoFileProvider.getUriForFile(photoFile)
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
         intent.putExtra(MediaStore.EXTRA_OUTPUT, photoFileUri)
         fragment.startActivityForResult(intent, requestCode)
