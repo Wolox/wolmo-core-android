e7a8c84c4ef248b02163dddc8ac1641cfdae29d0
Dylan Muszel <dylan.muszel@wolox.com.ar>
2019-12-27 13:04:06 -0300

    Updating kapt

--- core/src/main/java/ar/com/wolox/wolmo/core/di/modules/ContextModule.kt
+++ core/src/main/java/ar/com/wolox/wolmo/core/di/modules/ContextModule.kt
@@ -19,34 +19,31 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package ar.com.wolox.wolmo.core.di.modules;
+package ar.com.wolox.wolmo.core.di.modules
 
-import android.app.Activity;
-import android.app.Application;
-import android.content.Context;
-import android.content.SharedPreferences;
-
-import ar.com.wolox.wolmo.core.di.scopes.ApplicationScope;
-
-import dagger.Module;
-import dagger.Provides;
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import ar.com.wolox.wolmo.core.di.scopes.ApplicationScope
+import dagger.Module
+import dagger.Provides
 
 /**
- * Provides objects that depends of the Application {@link Context}.
- * The objects provided by this module uses {@link ApplicationScope}.
+ * Provides objects that depends of the Application [Context].
+ * The objects provided by this module uses [ApplicationScope].
  */
 @Module
-public class ContextModule {
+class ContextModule {
+    @Provides
+    @ApplicationScope
+    fun provideContext(application: Application): Context {
+        return application.applicationContext
+    }
 
-	@Provides
-	@ApplicationScope
-	Context provideContext(Application application) {
-		return application.getApplicationContext();
-	}
-
-	@Provides
-	@ApplicationScope
-	SharedPreferences provideSharedPreferences(String sharedPrefName, Context context) {
-		return context.getSharedPreferences(sharedPrefName, Activity.MODE_PRIVATE);
-	}
+    @Provides
+    @ApplicationScope
+    fun provideSharedPreferences(sharedPrefName: String?, context: Context): SharedPreferences {
+        return context.getSharedPreferences(sharedPrefName, Activity.MODE_PRIVATE)
+    }
 }
