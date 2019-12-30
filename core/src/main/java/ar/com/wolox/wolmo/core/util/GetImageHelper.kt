01d9a3e847a418e26559949edb7a1c25e8eb34ac
Dylan Muszel <dylan.muszel@wolox.com.ar>
2019-12-30 15:46:47 -0300

    More comments

--- core/src/main/java/ar/com/wolox/wolmo/core/util/GetImageHelper.kt
+++ core/src/main/java/ar/com/wolox/wolmo/core/util/GetImageHelper.kt
@@ -6,25 +6,30 @@ import ar.com.wolox.wolmo.core.permission.PermissionListener
 import ar.com.wolox.wolmo.core.permission.PermissionManager
 import javax.inject.Inject
 
+/**
+ * A helper class to open gallery and camera to get an image.
+ */
 class GetImageHelper @Inject constructor(
     private val permissionManager: PermissionManager,
     private val imageProvider: ImageProvider
 ) {
 
-    fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        permissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
+    /**
+     * Open the gallery from the given [fragment]. Override [Fragment.onActivityResult] and check
+     * given [code] to get result. [onPermissionDenied] will be invoked if camera gallery
+     * permissions have been denied and [onGalleryNotFound] will be invoked if there's no
+     * gallery application on the device.
+     */
     fun openGallery(
         fragment: Fragment,
         code: Int,
         onPermissionDenied: () -> Unit = {},
-        onError: () -> Unit = {}
+        onGalleryNotFound: () -> Unit = {}
     ) {
         permissionManager.requestPermission(fragment.requireActivity(), object : PermissionListener() {
             override fun onPermissionsGranted() {
                 if (!imageProvider.getImageFromGallery(fragment, code)) {
-                    onError()
+                    onGalleryNotFound()
                 }
             }
 
@@ -34,18 +39,24 @@ class GetImageHelper @Inject constructor(
         }, *GALLERY_PERMISSIONS)
     }
 
+    /**
+     * Open the camera from the given [fragment] and save the picture into [destinationFilename].
+     * Override [Fragment.onActivityResult] and check given [code] to get result.
+     * [onPermissionDenied] will be invoked if camera gallery permissions have been
+     * denied and [onCameraNotFound] will be invoked if there's no camera application on the device.
+     */
     fun openCamera(
         fragment: Fragment,
         code: Int,
         destinationFilename: String,
         onPermissionDenied: () -> Unit = {},
-        onError: () -> Unit = {}
+        onCameraNotFound: () -> Unit = {}
     ) {
 
         permissionManager.requestPermission(fragment.requireActivity(), object : PermissionListener() {
             override fun onPermissionsGranted() {
                 if (!imageProvider.getImageFromCamera(fragment, code, destinationFilename)) {
-                    onError()
+                    onCameraNotFound()
                 }
             }
 
